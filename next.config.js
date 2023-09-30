7fc06a34ae2eed6636943c47f889d634867cf581
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-09-30 12:17:35 -0400

    algo

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     // experimental: {
     //     serverActions: true,
     //   },
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+      // Important: return the modified config
+      return config
+    },
+  
       eslint: {
         ignoreDuringBuilds: true,
       },
