50249cf7af6d8c0b388ed599be6ef0013499217d
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-09-30 20:55:57 -0400

    algo

--- src/app/components/InputNav.jsx
+++ src/app/components/InputNav.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import {LiaSearchSolid} from 'react-icons/lia'
 import { useRouter, redirect } from 'next/navigation'
 import Search from '../actions/Search'
+import { Button } from '@nextui-org/react'
 
 
 function InputNav() {
@@ -14,16 +15,20 @@ function InputNav() {
       redirect(`/productos/search/${search}`)
     }
   return (
-    <form onSubmit={() => router.push(`productos/search/${search}`)} className='nav-link-search'>
-    {/* // <form action={Search(search)} className='nav-link-search'> */}
-                  <div className="search-nav">
-                    <form style={{paddingBottom: '0'}} name="search search-relative" className=" search-relative-nav">
-                        <input type="text" className="input-search-nav "  onChange={(e) => setSearch(e.target.value)}/>
-                        {/* <input type="text" className="input-search-nav " onChange={(e) => setSearchQuery(e.target.value)} name="txt"  /> */}
-                        {/* <LiaSearchSolid fontSize={25} style={{top:'50%'}}className="search-button-nav" /> */}
-                    </form>
+    
+
+
+    <div  className='nav-link-search'>
+        {/* // <form action={Search(search)} className='nav-link-search'> */}
+                      <div className="search-nav flex items-center gap-2">
+                        <form onSubmit={() => router.push(`/productos/search/${search}`)} style={{paddingBottom: '0'}}  name="search search-relative" className=" search-relative-nav">
+                            <input type="text" className="input-search-nav "  onChange={(e) => setSearch(e.target.value)}/>
+                            {/* <input type="text" className="input-search-nav " onChange={(e) => setSearchQuery(e.target.value)} name="txt"  /> */}
+                            {/* <LiaSearchSolid fontSize={25} style={{top:'50%'}}className="search-button-nav" /> */}
+                        </form>
+                        <Button className='m-0' onClick={() => router.push(`/productos/search/${search}`)} color='warning' variant='ghost'>Buscar</Button>
+                      </div>
                   </div>
-              </form>
   )
 }
 
