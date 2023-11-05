18ce3031551c023162ebbe475d15d6c2ad0b49e5
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-11-05 19:43:47 -0400

    algo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,10 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <Suspense fallback={<div>Cargando...</div>}>
+      {/* <Suspense fallback={<div>Cargando...</div>}>
 
-        {/* <Cards />                       */}
         <Cards2 />   
-      </Suspense>
+      </Suspense> */}
        <Categories />
       {/* <Gallery /> */}
       <Grid /> 
